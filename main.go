8a173c805adec0ccda20c0c6eeee3fb9491fd687
kpenfound <kyle@dagger.io>
2022-09-14 15:01:25 -0400

    project init
    Signed-off-by: kpenfound <kyle@dagger.io>

---
+++ main.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Khan/genqlient/graphql"
+	"github.com/dagger/cloak/sdk/go/dagger"
+)
+
+func (r *terraform) apply(ctx context.Context, config dagger.FSID, token dagger.SecretID) (*dagger.Filesystem, error) {
+	cmd := []string{"terraform", "apply", "-auto-approve"}
+	return tfExec(ctx, config, token, cmd)
+}
+
+func (r *terraform) plan(ctx context.Context, config dagger.FSID, token dagger.SecretID) (*dagger.Filesystem, error) {
+	cmd := []string{"terraform", "plan"}
+	return tfExec(ctx, config, token, cmd)
+}
+
+func (r *terraform) fmt(ctx context.Context, config dagger.FSID, token dagger.SecretID) (*dagger.Filesystem, error) {
+	cmd := []string{"terraform", "fmt"}
+	return tfExec(ctx, config, token, cmd)
+}
+
+func (r *terraform) destroy(ctx context.Context, config dagger.FSID, token dagger.SecretID) (*dagger.Filesystem, error) {
+	cmd := []string{"terraform", "apply", "-destroy", "-auto-approve"}
+	return tfExec(ctx, config, token, cmd)
+}
+
+func tfExec(ctx context.Context, config dagger.FSID, token dagger.SecretID, command []string) (*dagger.Filesystem, error) {
+	client, err := dagger.Client(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	fsid, err := image(ctx, client, "hashicorp/terraform:latest")
+	if err != nil {
+		fmt.Printf("cant load image: %v", err)
+		return nil, err
+	}
+
+	execid, err := exec(ctx, client, fsid, config, token, command)
+	if err != nil {
+		fmt.Printf("cant execute plan: %v", err)
+		return nil, err
+	}
+
+	return &dagger.Filesystem{ID: execid}, nil
+}
+
+func image(ctx context.Context, client graphql.Client, ref string) (dagger.FSID, error) {
+	req := &graphql.Request{
+		Query: `
+query Image ($ref: String!) {
+	core {
+		image(ref: $ref) {
+			id
+		}
+	}
+}
+`,
+		Variables: map[string]any{
+			"ref": ref,
+		},
+	}
+	resp := struct {
+		Core struct {
+			Image struct {
+				ID dagger.FSID
+			}
+		}
+	}{}
+	err := client.MakeRequest(ctx, req, &graphql.Response{Data: &resp})
+	if err != nil {
+		return "", err
+	}
+
+	return resp.Core.Image.ID, nil
+}
+
+func exec(ctx context.Context, client graphql.Client, root dagger.FSID, mount dagger.FSID, token dagger.SecretID, args []string) (dagger.FSID, error) {
+	req := &graphql.Request{
+		Query: `
+query TfExec ($root: FSID!, $mount: FSID!, $args: [String!]!, $token: SecretID!) {
+	core {
+		filesystem(id: $root) {
+			exec(input: {
+				args: $args,
+				workdir: "/src",
+				mounts: [
+					{
+						path: "/src",
+						fs: $mount
+					}
+				],
+				secretEnv: [
+					{
+						name: "TF_TOKEN_app_terraform_io",
+						id: $token
+					}
+				]
+			}) {
+				fs {
+					id
+				}
+			}
+		}
+	}
+}
+`,
+		Variables: map[string]any{
+			"root":  root,
+			"mount": mount,
+			"args":  args,
+			"token": token,
+		},
+	}
+	resp := struct {
+		Core struct {
+			Filesystem struct {
+				Exec struct {
+					FS struct {
+						ID dagger.FSID
+					}
+				}
+			}
+		}
+	}{}
+	err := client.MakeRequest(ctx, req, &graphql.Response{Data: &resp})
+	if err != nil {
+		return "", err
+	}
+
+	return resp.Core.Filesystem.Exec.FS.ID, nil
+}
